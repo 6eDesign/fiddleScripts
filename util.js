82bbab6b4954be30e375747e9c50275750405f74
Jonathan Greenemeier <jon@6eDesign.net>
2015-03-20 23:17:00 -0600

    Init Commit

---
+++ util.js
@@ -0,0 +1,6 @@
+var util = (function(w,d,$,pub){
+    pub.injectStringData = function(str,name,value) { 
+            return str.replace(new RegExp('#{'+name+'}','g'),value); 
+	        }; 
+		    return pub; 
+		    })(window,document,jQuery,{}); 
