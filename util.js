520a4817f50784568ca3f945052e80b7ab439023
Jonathan Greenemeier <jon@6eDesign.net>
2015-03-20 23:48:46 -0600

    Removed poorly placed files

--- util.js
+++
@@ -1,6 +0,0 @@
-var util = (function(w,d,$,pub){
-  pub.injectStringData = function(str,name,value) { 
-    return str.replace(new RegExp('#{'+name+'}','g'),value); 
-  }; 
-return pub; 
-})(window,document,jQuery,{}); 
